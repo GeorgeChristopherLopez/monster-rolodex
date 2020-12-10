ea1aeda28997337b19be6851738ddb93e3a4fd80
GeorgeChristopherLopez <georgechristopherlopez@gmail.com>
2020-12-10 18:06:55 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import  {CardList} from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component';
 
+ 
 function App() {
+const [searchField, setSearchFeild] = useState("");
+const [monsters, setMonsters] = useState([]);
+const filteredMonsters = monsters.filter(monster=>
+  monster.name.toLowerCase().includes(searchField.toLowerCase()))
+
+// Similar to componentDidMount and componentDidUpdate:
+useEffect(() => {
+  // Update the document title using the browser API
+  
+// console.log("componentDidMount or componentDidUpdated ")
+  fetch('https://jsonplaceholder.typicode.com/users/')
+  .then(response => response.json())
+  .then(users => setMonsters(users))
+},[]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Monsters Rolodex</h1>
+    <SearchBox placeholder="search for stuff" handleChange={(e)=>setSearchFeild(e.target.value)}/>
+    <CardList monsters={filteredMonsters}>   
+    </CardList>
+     </div>
+
   );
 }
 
